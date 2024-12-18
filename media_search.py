0d4d39efdc6b7e06d28642be3c6b48af55486dae
Nguyen Thanh Tinh <ngthanhtinh801@gmail.com>
2024-12-18 18:25:34 +0700

    Add random GIF selection

--- media_search.py
+++ media_search.py
@@ -1,21 +1,30 @@
 import os
 import requests
+import random
 
 async def search_and_send_gif(ctx, query):
     """
-    Tìm kiếm GIF từ Tenor API và gửi kết quả về Discord.
+    Tìm kiếm GIF từ Tenor API, lấy 30 kết quả và gửi ngẫu nhiên một GIF vào Discord
     """
+    
+    finalQuery = "anime" + " " + query
+    
     try:
         
         TENOR_API_KEY = os.getenv('tenor_api_key')
-        TENOR_API_URL = f"https://tenor.googleapis.com/v2/search?q={query}&key={TENOR_API_KEY}&limit=1"
+        TENOR_API_URL = f"https://tenor.googleapis.com/v2/search?q={finalQuery}&key={TENOR_API_KEY}&limit=20"
         
         response = requests.get(TENOR_API_URL)
         if response.status_code == 200:
             data = response.json()
-            if data.get("results"):
-                gif_url = data["results"][0]["media_formats"]["gif"]["url"]
-                print(gif_url)
+            results = data.get("results", [])
+            if results:
+                # Chọn ngẫu nhiên một GIF từ danh sách kết quả
+                random_gif = random.choice(results)
+                gif_url = random_gif["media_formats"]["gif"]["url"]
+                
+                print(f"Selected GIF URL: {gif_url}")
+                
                 await ctx.send(gif_url)
             else:
                 await ctx.send("Không tìm thấy hình ảnh hoặc GIF nào khớp với keyword")
